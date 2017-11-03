34b896ca8b421d7ff8d3674c19e0d5f1cb1a0d91
tyfoan <alexanderogurtsov@Alexanders-MacBook-Pro.local>
2017-11-03 17:47:36 +0200

    redux init implementation

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -10,6 +10,11 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
